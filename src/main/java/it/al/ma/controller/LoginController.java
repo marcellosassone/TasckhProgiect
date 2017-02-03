0eeb89bf29287dc87f209f9d375f3e8cb7359c66
dausilio-alessandra <dausilio.alessandra@gmail.com>
2017-02-03 16:04:11 +0100

    1commit

--- src/main/java/it/al/ma/controller/LoginController.java
+++ src/main/java/it/al/ma/controller/LoginController.java
@@ -13,7 +13,7 @@ import it.al.ma.dao.UserDaoImpl;
 import it.al.ma.model.User;
 
 
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 @Controller
 public class LoginController {
 
